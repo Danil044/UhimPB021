7d7f7305ae15787bd5dec8e8c351726cd2b87b8d
Danil044 <79462734+Danil044@users.noreply.github.com>
2021-02-22 17:48:33 +0300

    Dz7
    Dz7

---
+++ DZ7/DZ7.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Animal
+{
+protected :
+	string name;
+public:
+	Animal(string nema) : name(name) {}
+
+	virtual void Say() = 0;
+
+};
+
+class Dog : public Animal
+{
+public:
+	Dog(string name ) : Animal(name) {}
+
+	void Say() override
+	{
+		cout << name << "GAV-GAV" << endl;
+	}
+};
+
+class Cat : public Animal
+{
+public: 
+	Cat(string name) : Animal(name) {}
+
+	void Say() override
+	{
+		cout << name << "MAY-MAY" << endl;
+	}
+};
+
+class Donald_Duck : public Animal
+{
+public:
+	Donald_Duck(string name) : Animal(name) {}
+    
+	void Say() override
+	{
+		cout << name << "KREY-KREY" << endl;
+	}
+};
+
+int main()
+{
+	Animal* animals[3] = { new Dog("Dog") , new Cat("Cat") , new Donald_Duck("Donald Duck") };
+
+	for (int i = 0; i < 3; i++)
+	{
+		animals[i]->Say();
+   }
+
+	return 0;
+
+
+}
